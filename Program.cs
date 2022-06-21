6aa4246e52e655d0299c167bddeb232a7aeb6d23
salt developer <salt-dev@DEV144.localdomain>
2022-06-21 12:01:15 +0200

    Added two help functions

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class MainClass {
+
+  public static string LongestWord(string sen) {
+  
+    return GetLongestWord(RemoveSigns(sen));
+
+  }
+
+  public static string GetLongestWord(string sen) {
+    var Length = 0;
+    var TheLongestWord ="";
+    var senArray = sen.Split(" ");
+    foreach (var Word in senArray) {
+      if (Word.Length > Length) {
+        Length = Word.Length;
+        TheLongestWord = Word;
+      }
+    }
+    return TheLongestWord;
+  }
+
+  public static string RemoveSigns (string sen) {
+    var result = "";
+    for (var i = 0; i<sen.Length; i++) {
+      if (char.IsDigit(sen[i]) || char.IsLetter(sen[i]) || sen[i] == ' ') {
+        result += sen[i];
+      }
+    }
+    return result;
+  }
+
+
+  static void Main() {  
+    // keep this function call here
+    Console.WriteLine(LongestWord("I love dogs"));
+    Console.ReadLine();
+  } 
+
+}
